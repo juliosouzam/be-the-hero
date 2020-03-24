8c779582100281d54746d28e158445e7486b8ea6
Júlio César <julio.souzam@gmail.com>
2020-03-24 13:48:08 -0300

    Backend application Be The Hero

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,66 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const { page = 1 } = req.query;
+
+      const [count] = await connection('incidents').count();
+
+      const incidents = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select([
+          'incidents.*',
+          'ongs.name',
+          'ongs.email',
+          'ongs.whatsapp',
+          '.ongs.city',
+          'ongs.uf',
+        ]);
+
+      res.header('X-Total-Count', count['count(*)']);
+
+      return res.json(incidents);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  },
+  async store(req, res) {
+    try {
+      const { title, description, value } = req.body;
+      const ong_id = req.headers.authorization;
+
+      const [id] = await connection('incidents').insert({
+        title,
+        description,
+        value,
+        ong_id,
+      });
+
+      return res.json({ id });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  },
+  async destroy(req, res) {
+    const { id } = req.params;
+    const ong_id = req.headers.authorization;
+
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first();
+
+    if (incident.ong_id !== ong_id) {
+      return res.status(401).json({ error: 'Operation not allowed' });
+    }
+
+    await connection('incidents')
+      .where('id', id)
+      .delete();
+
+    return res.status(204).json();
+  },
+};
